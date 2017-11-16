24271addb37eef189edcb355d4f4e189d427b5ad
Skial Bainn <skial.bainn@gmail.com>
2017-11-16 14:54:16 +0000

    Add macro tests to directly test TDynamic instead of trying to write code that will generate it.

--- tests/Main.hx
+++ tests/Main.hx
@@ -7,7 +7,8 @@ class Main {
 
     public static function main() {
         var runner = new Runner();
-        runner.addCase (new DefaultSpec() );
+        runner.addCase( new DefaultSpec() );
+        runner.addCase( new DefaultMacroSpec() );
         Report.create( runner );
         runner.run();
     }
